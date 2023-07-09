bd3bf03756c6854462fe2eb7bae0adb00d8dc335
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-07-08 22:28:17 -0400

    worked on schemas for the project

---
+++ models/Thought.js
@@ -0,0 +1,8 @@
+const {Schema, model, Types} = require('mongoose')
+
+const reactionSchema = new Schema({
+    reactionId:{
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId()
+    }
+})
