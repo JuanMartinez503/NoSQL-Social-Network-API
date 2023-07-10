91c06c4a77187272f89da520cba32ae8d76ae9d0
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-07-10 12:41:43 -0400

    I completed the models that will be used

--- models/Thought.js
+++ models/Thought.js
@@ -1,8 +1,50 @@
-const {Schema, model, Types} = require('mongoose')
+const { Schema, model, Types } = require("mongoose");
 
 const reactionSchema = new Schema({
-    reactionId:{
-        type: Schema.Types.ObjectId,
-        default: ()=> new Types.ObjectId()
-    }
-})
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    get: function (value) {
+      return new Date(value).toISOString();
+    },
+  },
+});
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    get: function (value) {
+      return new Date(value).toISOString();
+    },
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+});
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+const Thought = model("Thought", thoughtSchema);
+module.exports = Thought;
